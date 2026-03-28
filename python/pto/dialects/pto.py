cc18b84bf82500ffc58d4eae06c8ba97fdac4a94
FangRui <fangrui_95@163.com>
2026-03-28 14:20:11 +0800

    feat: add OAT.3 license header
    Signed-off-by: FangRui <fangrui_95@163.com>

--- python/pto/dialects/pto.py
+++ python/pto/dialects/pto.py
@@ -1,6 +1,11 @@
-#  Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-#  See https://llvm.org/LICENSE.txt for license information.
-#  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+# Copyright (c) 2026 Huawei Technologies Co., Ltd.
+# This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+# CANN Open Software License Agreement Version 2.0 (the "License").
+# Please refer to the License for details. You may not use this file except in compliance with the License.
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+# See LICENSE in the root of the software repository for the full text of the License.
+
 from . import _pto_ops_gen as _pto_ops_gen
 from ._pto_ops_gen import *
 from mlir import ir as _ods_ir
